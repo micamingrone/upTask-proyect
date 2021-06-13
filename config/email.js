a95e849cce3db99803d14ce0ae77c5fb52a4324d
Mica Mingrone <micacp09@gmail.com>
2021-06-13 20:39:56 -0300

    .

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: 'postmaster@sandbox5c1eef9c4257446882d29e4a45e7d3af.mailgun.org',
-    pass: 'a4ae5191b68b12022e60353b95a48090-90ac0eb7-eb280c8f',
-    host: 'smtp.mailgun.org',
-    port: '587'
+    user: 'fe912c231b95dd',
+    pass: 'cb4a2be40a4db5',
+    host: 'smtp.mailtrap.io',
+    port: '2525'
 }
